86d9a08f03e6590b9b6a6217116eee93c4021f69
Md Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2024-01-11 23:49:42 +0600

    first program : with type

--- first-program/index.js
+++ first-program/index.js
@@ -1,2 +1,10 @@
 var userName = "monir ";
 console.log(userName);
+var total = 5;
+total = total + 3;
+console.log(total);
+function getSum(num1, num2) {
+    console.log("sum = ", num1 + num2);
+}
+getSum(5, 5);
+getSum(5, 56);
--- first-program/index.ts
+++ first-program/index.ts
@@ -1,3 +1,16 @@
 let userName = "monir ";
 
 console.log(userName);
+
+let total: number = 5;
+
+total = total + 3;
+
+console.log(total);
+
+function getSum(num1: number, num2: number) {
+  console.log("sum = ", num1 + num2);
+}
+
+getSum(5, 5);
+getSum(5, 56);
