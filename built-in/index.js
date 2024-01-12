f46bacd371d93fd2fbe3970390f50a31220a857d
Md Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2024-01-13 00:18:12 +0600

    built in data type added

---
+++ built-in/index.js
@@ -0,0 +1,14 @@
+// number type
+var age = 20;
+console.log("number = ", age);
+// string type
+var idName = "noob bot ";
+console.log("your name = ", idName.toUpperCase());
+// boolean type
+var isIdle = true;
+console.log("is idle = ", isIdle);
+// void type function
+function sayHello() {
+    console.log("hello world ");
+}
+sayHello();
---
+++ built-in/index.ts
@@ -0,0 +1,20 @@
+// number type
+let age: number = 20;
+
+console.log("number = ", age);
+
+// string type
+let idName: String = "noob bot ";
+console.log("your name = ", idName.toUpperCase());
+
+// boolean type
+let isIdle: boolean = true;
+
+console.log("is idle = ", isIdle);
+
+// void type function
+function sayHello(): void {
+  console.log("hello world ");
+}
+
+sayHello();
